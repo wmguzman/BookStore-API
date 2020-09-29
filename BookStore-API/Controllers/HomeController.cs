65ef0c7f3ef38c0abdef173c980fb14b1f6ccd36
Mauricio Guzman <wg1009131@hotmail.com>
2020-09-28 20:01:26 -0500

    avance

---
+++ BookStore-API/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStore_API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BookStore_API.Controllers
+{
+
+    /// <summary>
+    ///  Este es un controlador de prueba
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        //Dependency injection del servicio ILoggerService para usar el logger
+        private readonly ILoggerService _logger;
+
+        public HomeController(ILoggerService logger)
+        {
+            _logger = logger;
+        }
+        /// <summary>
+        ///  una función para Get
+        /// </summary>
+        /// <returns></returns>
+        // GET: api/<HomeController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            _logger.LogInfo("Se accedió a la función Get de Homecontroller");
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<HomeController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            _logger.LogWarn("Se accedió a la función Get de Homecontroller con un valor");
+            return "value";
+        }
+
+        // POST api/<HomeController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            _logger.LogDebug("Se accedió a la función Post de Homecontroller");
+        }
+
+        // PUT api/<HomeController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<HomeController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ BookStore-API/Startup.cs
@@ -0,0 +1,109 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using BookStore_API.Data;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+using System.IO;
+using BookStore_API.Services;
+
+namespace BookStore_API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            //Aquí se configura CORS
+            services.AddCors(o =>
+            {
+                o.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyMethod());
+            });
+
+            //Aquí se configura swashbuckler swager que se acaba de instalar 3 modulos usando nuget
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("v1", new OpenApiInfo {
+                    Title = "Book Store API",
+                    Version = "v1",
+                    Description = "Prueba inicial de esta API"
+                });
+                var xmlfile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlpath = Path.Combine(AppContext.BaseDirectory, xmlfile);
+                c.IncludeXmlComments(xmlpath);
+            });
+
+            //Aquí se inicia el logger que se creó con NLog
+            services.AddSingleton<ILoggerService, LoggerService>();
+
+            services.AddControllers();
+            //services.AddRazorPages();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            //Aquí se configura swashbuckler swager que se acaba de instalar 3 modulos usando nuget
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Book Store API");
+                c.RoutePrefix = "";
+
+            });
+
+            app.UseHttpsRedirection();
+            //app.UseStaticFiles();
+
+            //Aquí usamos el CORS que se configuró antes
+            app.UseCors("CorsPolicy");
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                //endpoints.MapRazorPages();
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
